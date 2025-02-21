f4e543c21defe1aba81a2073635f93a6154b110c
tintayadev <pau.tintaya.code@gmail.com>
2025-02-20 22:06:30 -0400

    feat: add i18n service for language tranlations

--- src/app/services/i18n.service.ts
+++ src/app/services/i18n.service.ts
@@ -1,9 +1,50 @@
-import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { ApplicationRef, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class I18nService {
 
-  constructor() { }
+  private data: any = {};
+  private currentLanguage: string = 'en';
+
+  constructor(private http: HttpClient, private ref: ApplicationRef) {
+    this.setLanguage(localStorage.getItem('lang') || 'en');
+  }
+
+  public setLanguage(lang: string): void {
+    localStorage.setItem('lang', lang);
+    this.currentLanguage = lang;
+    this.loadTranslations(lang);
+  }
+
+  public getTranslation(phrase: string, variables: any=null): string {
+    if (this.data[phrase]) {
+      let translated = this.data[phrase];
+      if (variables) {
+        Object.keys(variables).forEach((key) => {
+          translated = translated.replace(`{${key}}`, variables[key]);
+        });
+      }
+      return translated;
+    }
+    return phrase;
+  }
+
+  private loadTranslations(lang: string):void {
+    const langFilePath = `assets/langs/${lang}.json`;
+    this.http.get(langFilePath).subscribe({
+      next: (data: any) => {
+        this.data = data;
+        this.ref.tick();
+      },
+      error: (error) => {
+        console.error(`Error loading language file: ${langFilePath}`, error);
+      }
+    });
+
+
+  }
+
 }

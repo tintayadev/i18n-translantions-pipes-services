e2c6bf4ba2106f641b85c5e6566b0b3d3fc0c1c2
tintayadev <pau.tintaya.code@gmail.com>
2025-02-20 22:07:40 -0400

    feat: add language switcher component

--- src/app/components/language-switcher/language-switcher.component.ts
+++ src/app/components/language-switcher/language-switcher.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { I18nService } from '../../services/i18n.service';
+import { CommonModule } from '@angular/common';
+import { I18nPipe } from '../../pipes/i18n.pipe';
 
 @Component({
   selector: 'app-language-switcher',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, I18nPipe],
   templateUrl: './language-switcher.component.html',
   styleUrl: './language-switcher.component.scss'
 })
 export class LanguageSwitcherComponent {
+  constructor(private i18nService: I18nService) {}
+
+  changeLng(lang: string) {
+    this.i18nService.setLanguage(lang);
+  }
 
 }

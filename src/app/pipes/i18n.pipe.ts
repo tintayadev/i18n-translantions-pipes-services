5573d191d74d7ae87032029acdfd8a98f8368d2f
tintayadev <pau.tintaya.code@gmail.com>
2025-02-20 22:07:06 -0400

    feat: implement i18n pipe for transnlations

--- src/app/pipes/i18n.pipe.ts
+++ src/app/pipes/i18n.pipe.ts
@@ -1,12 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { I18nService } from '../services/i18n.service';
 
 @Pipe({
-  name: 'i18n'
+  name: 'i18n',
+  standalone: true,
+  pure: false, // To ensure udpates when language changes
 })
 export class I18nPipe implements PipeTransform {
+  constructor(private i18nService: I18nService) {}
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(phrase: string, variables?: any): string {
+    return this.i18nService.getTranslation(phrase, variables);
   }
 
 }

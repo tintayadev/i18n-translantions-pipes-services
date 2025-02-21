8d92a58131061af748e183009aa5877643db80b5
tintayadev <pau.tintaya.code@gmail.com>
2025-02-20 22:08:06 -0400

    feat: add home component for translation testing

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { I18nPipe } from '../../pipes/i18n.pipe';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, I18nPipe, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  name: string= '';
 }
